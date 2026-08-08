f88f8dcbb8748882ffa2fe8cdf44938bb238282f
Venkata Bharath <165052159+DVBharath2005@users.noreply.github.com>
2026-08-08 11:42:46 +0530

    Update image URL in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -31,7 +31,7 @@ export const PROJECTS: Project[] = [
       'AWS'
     ],
     githubUrl: 'https://github.com/bharath-ecommerce-ai',
-    image: 'https://res.cloudinary.com/dtz0urit6/image/upload/q_auto:best,f_jpg/cloudinary-tools-uploads/h7lq60hr2ngfb8qhw3nq',
+    image: 'https://ibb.co/ZpgjQFhP',
     details: [
       'Developed a full-featured e-commerce platform supporting thousands of users with high availability',
       'Integrated secure payment processing using Stripe and automated billing workflows',
